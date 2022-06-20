21928b2aa60a0b04d3e71865cac401a3271ce33d
chang wong <changcwong@gmail.com>
2022-06-19 17:37:17 -0700

    further testing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 window.prompt('Do you head left or right?')
 let answer = window.prompt('Do you head left or right?')
 console.log(firstAnswer)
+let firstAnswer  = window.prompt('Do you head left or right?')
+console.log(firstAnswer === 'left')
